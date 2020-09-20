f1c2f8167025ca3e278205d3c1bef7f86fb5a3db
Marc van Deuren <marcvandeuren@gmail.com>
2020-09-20 13:53:27 +0200

    Added properties to ModifierBehaviour

--- Runtime/Syc/Combat/ModifierSystem/ScriptableObjects/ModifierBehaviour.cs
+++ Runtime/Syc/Combat/ModifierSystem/ScriptableObjects/ModifierBehaviour.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Syc.Combat.ModifierSystem.ScriptableObjects
 {
 	public class ModifierBehaviour : ScriptableObject
 	{
+		public string ModifierName => modifierName;
+		public string ModifierDescription => modifierDescription;
+		public Image Icon => icon;
 		public float Duration => duration;
-		
-		[SerializeField]
-		protected float duration;
 
+		[SerializeField] protected string modifierName;
+
+		[SerializeField] protected string modifierDescription;
+		
+		[SerializeField] protected Image icon;
+
+		[SerializeField] protected float duration;
 	}
 }
