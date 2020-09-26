450c918a0a63a540656bdeedea4194a0df9e4c58
Marc van Deuren <marcvandeuren@gmail.com>
2020-09-26 17:21:02 +0200

    added prefabs

---
+++ Runtime/Syc/Combat/SpellSystem/SpellObjects/AssetCreateMenu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Syc.Combat.SpellSystem.SpellObjects
+{
+    public static class AssetCreateMenu
+    {
+        [MenuItem ("Assets/Create/Spell System/Spells/Spell Objects/Projectile Prefab")]
+        public static void CreateProjectilePrefab()
+        {
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var fileName = "/Projectile";
+            var fileExtension = ".prefab";
+
+            var fileExists = AssetDatabase.LoadAssetAtPath<Object>(path + fileName + fileExtension) != null;
+            
+            Debug.Log("Hoi");
+            Object source = Resources.Load("ProjectilePrefab");
+            if (source == null)
+            {
+                Debug.Log("source was null");
+                return;
+            }
+            GameObject objSource = (GameObject)PrefabUtility.InstantiatePrefab(source);
+            if (objSource == null)
+            {
+                Debug.Log("objSource was null");
+                return;
+            }
+
+            var writeTo = fileExists
+                ? path + fileName + Guid.NewGuid() + fileExtension
+                : path + fileName + fileExtension;
+            
+            GameObject obj = PrefabUtility.SaveAsPrefabAsset(objSource, writeTo);
+            
+            Object.DestroyImmediate(objSource);
+        }
+        
+        [MenuItem ("Assets/Create/Spell System/Spells/Spell Objects/Burst Particles Prefab")]
+        public static void CreateBurstParticlesPrefab()
+        {
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var fileName = "/BurstParticles";
+            var fileExtension = ".prefab";
+
+            var fileExists = AssetDatabase.LoadAssetAtPath<Object>(path + fileName + fileExtension) != null;
+            
+            Debug.Log("Hoi");
+            Object source = Resources.Load("BurstParticlesPrefab");
+            if (source == null)
+            {
+                Debug.Log("source was null");
+                return;
+            }
+            GameObject objSource = (GameObject)PrefabUtility.InstantiatePrefab(source);
+            if (objSource == null)
+            {
+                Debug.Log("objSource was null");
+                return;
+            }
+
+            var writeTo = fileExists
+                ? path + fileName + Guid.NewGuid() + fileExtension
+                : path + fileName + fileExtension;
+            
+            GameObject obj = PrefabUtility.SaveAsPrefabAsset(objSource, writeTo);
+            
+            Object.DestroyImmediate(objSource);
+        }
+    }
+}
---
+++ Runtime/Syc/Combat/SpellSystem/SpellObjects/ParticleBurst.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Syc.Combat.SpellSystem.SpellObjects
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class ParticleBurst : SpellObject
+    {
+        private ParticleSystem _particleSystem;
+        private void Awake()
+        {
+            _particleSystem = GetComponent<ParticleSystem>();
+            _particleSystem.Play();
+            Destroy(gameObject, _particleSystem.main.duration);
+        }
+        
+    }
+}
