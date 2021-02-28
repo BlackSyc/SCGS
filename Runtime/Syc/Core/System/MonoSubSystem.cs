6058f086f5eb9d871518a01479dac849374841b3
Marc van Deuren <marcvandeuren@gmail.com>
2021-02-28 15:27:01 +0100

    added temporary networked systems

--- Runtime/Syc/Core/System/MonoSubSystem.cs
+++ Runtime/Syc/Core/System/MonoSubSystem.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using MLAPI;
+using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
 
 namespace Syc.Core.System
 {
@@ -8,6 +10,16 @@ namespace Syc.Core.System
 	}
 
 	public abstract class MonoSubSystem<T> : MonoSubSystem, ISubSystem<T>
+	{
+		public abstract T System { get; set; }
+	}
+
+	public abstract class NetworkedMonoSubSystem : NetworkedBehaviour, ISubSystem
+	{
+		public virtual void Tick(float deltaTime){}
+	}
+
+	public abstract class NetworkedMonoSubsystem<T> : NetworkedMonoSubSystem, ISubSystem<T>
 	{
 		public abstract T System { get; set; }
 	}
--- Runtime/Syc/Core/System/MonoSystemBase.cs
+++ Runtime/Syc/Core/System/MonoSystemBase.cs
@@ -54,4 +54,55 @@ namespace Syc.Core.System
 	{
 		public T System { get; set; }
 	}
+
+	public class NetworkedMonoSystemBase : NetworkedMonoSubSystem, ISystemBase
+	{
+		private List<ISubSystem> _subsystems = new List<ISubSystem>();
+
+		public T Get<T>()
+		{
+			return _subsystems.OfType<T>().SingleOrDefault();
+		}
+
+		public bool Has<T>(out T t)
+		{
+			t = Get<T>();
+			return t != null;
+		}
+
+		public bool Has<T>()
+		{
+			return _subsystems.OfType<T>().Any();
+		}
+
+		private void Update()
+		{
+			if (_subsystems == default || !_subsystems.Any())
+				return;
+
+			var deltaTime = Time.deltaTime;
+			foreach (var subSystem in _subsystems)
+			{
+				subSystem.Tick(deltaTime);
+			}
+		}
+
+		protected void AddSubsystem(ISubSystem subSystem)
+		{
+			if (subSystem == null)
+				return;
+
+			_subsystems.Add(subSystem);
+		}
+		
+		protected void RemoveSubsystem(ISubSystem subSystem)
+		{
+			_subsystems.Remove(subSystem);
+		}
+	}
+	
+	public class NetworkedMonoSystemBase<T> : NetworkedMonoSystemBase, ISubSystem<T>
+	{
+		public T System { get; set; }
+	}
 }
